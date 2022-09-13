f4fd1b5c81c0ba44ee8711768a908860415c9ffe
Swati singh <swativishal8266@gmail.com>
2022-09-13 22:24:34 +0530

    login logic

--- login.js
+++ login.js
@@ -2,14 +2,14 @@
 
 
 
-let num=[2,3,4,5,6,8,9,10,11,12,13]
+let num=[2,3,4,5,6,8,9,10,11,12,13];
 let count=0;
     for(let i=0; i<=num; i++){
-        if(num%i=0){
+        if(num%i==0){
             count+;
         }
     }
-    if(count=2){
+    if(count==2){
         console.log("Yes");
     }else{
         console.log("No");
