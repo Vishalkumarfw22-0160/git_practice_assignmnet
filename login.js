6e760407df5a60e40b5a567df090ce34c3706526
Vishal Kumar <vishalkumar89588@gmail.com>
2022-09-13 21:33:53 +0530

    added identify prime number

---
+++ login.js
@@ -0,0 +1,16 @@
+//identifyPrime
+
+
+
+let num=[2,3,4,5,6,8,9,10,11,12,13]
+let count=0;
+    for(let i=0; i<=num; i++){
+        if(num%i=0){
+            count+;
+        }
+    }
+    if(count=2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
